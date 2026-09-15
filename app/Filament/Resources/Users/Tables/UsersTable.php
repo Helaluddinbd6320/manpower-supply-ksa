bcc28811ffeee55b2399fd8dcc0e77089056a7c6
Helal Uddin <helaluddinbd6320@gmail.com>
2026-09-15 12:20:16 +0300

    Add UserResource for staff management

---
+++ app/Filament/Resources/Users/Tables/UsersTable.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Filament\Resources\Users\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Tables\Table;
+
+class UsersTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->label('নাম')
+                    ->searchable()
+                    ->sortable(),
+
+                TextColumn::make('email')
+                    ->label('ইমেইল')
+                    ->searchable()
+                    ->sortable(),
+
+                TextColumn::make('roles.name')
+                    ->label('Role')
+                    ->badge()
+                    ->searchable(),
+
+                TextColumn::make('created_at')
+                    ->label('তৈরি হয়েছে')
+                    ->dateTime('d M Y')
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                SelectFilter::make('roles')
+                    ->label('Role')
+                    ->relationship('roles', 'name'),
+            ])
+            ->recordActions([
+                EditAction::make(),
+                DeleteAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
